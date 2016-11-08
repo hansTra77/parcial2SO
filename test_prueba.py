45b5634efb1b380becf498b498a50657dcfe569d
hansTra77 <jupame2006@hotmail.com>
2016-11-08 17:56:07 -0500

    Update test_prueba.py

--- test_prueba.py
+++ test_prueba.py
@@ -10,4 +10,3 @@ def test_get_files(client):
 	result = client.get('/v1.0/files',follow_redirects=True)
 	assert "prueba1" in result.data
 	assert "prueba2" in result.data
-	assert "prueba3" in result.data
